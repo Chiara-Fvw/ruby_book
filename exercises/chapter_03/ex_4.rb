adf4331a9bcd9dac86f217bc88dfadb8363aff43
Chiara Fiorentini <chiara.fvw@gmail.com>
2020-09-09 07:34:29 +0200

    more practicing

---
+++ exercises/chapter_03/ex_4.rb
@@ -0,0 +1,9 @@
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+#No devuelve nada porque "return" hace que Ruby salga del método y todo lo que hay detrás no se ejecuta.
