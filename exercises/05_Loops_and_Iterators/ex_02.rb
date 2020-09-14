e1dc4724f8349459f10cefa3d592626ced57c60e
Chiara Fiorentini <chiara.fvw@gmail.com>
2020-09-14 07:27:23 +0200

    more practice Loops

---
+++ exercises/05_Loops_and_Iterators/ex_02.rb
@@ -0,0 +1,21 @@
+word = gets.chomp
+word = word.upcase
+
+while (word !='STOP') do 
+  puts "Write the magic word"
+  word = gets.chomp
+  word = word.upcase
+end
+
+
+# book anser
+
+x = " "
+while x != "STOP" do
+  puts "Hi, How are you?"
+  ans = gets.chomp
+  puts "Want me to ask you again?"
+  x = gets.chomp
+end
+
+  
--- exercises/05_Loops_and_Iterators/practice_each.rb
+++ exercises/05_Loops_and_Iterators/practice_each.rb
@@ -1,3 +1,10 @@
 names = [ 'Bob', 'Joe', 'Steve', 'Janice', 'Susan', 'Helen']
+x = 1
 
-names.each { |name| puts name}
+# names.each { |name| puts name} one line block
+
+names.each do |name|
+  puts "#{x}. #{name}"
+  x += 1
+end
+
